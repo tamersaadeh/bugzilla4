3bc10059786fcd33810a0854dcb64a9e64390db5
Tamer Saadeh <tamer@tamersaadeh.com>
2011-04-21 19:15:06 +0300

    patched config.cgi

--- debian/maintenance/30_exit1onerror.sh
+++ debian/maintenance/30_exit1onerror.sh
@@ -407,7 +407,7 @@ diff -Naur bugzilla-srcdir.orig//colchange.cgi bugzilla-srcdir/colchange.cgi
 diff -Naur bugzilla-srcdir.orig//config.cgi bugzilla-srcdir/config.cgi
 --- bugzilla-srcdir.orig//config.cgi	2010-11-12 10:26:39.496917725 +0100
 +++ bugzilla-srcdir/config.cgi	2010-11-12 10:28:19.721916586 +0100
-@@ -161,5 +161,5 @@
+@@ -165,5 +165,5 @@
                              -type => $format->{'ctype'});
          print $output;
      }
