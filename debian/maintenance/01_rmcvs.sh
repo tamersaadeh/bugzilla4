f1afa0af1f689a505ad4dea3513f8ac2601df8b8
Tamer Saadeh <tamersaadeh@gmail.com>
2012-01-11 21:56:06 +0100

    removed useless lines, since they only use bzr

--- debian/maintenance/01_rmcvs.sh
+++ debian/maintenance/01_rmcvs.sh
@@ -4,8 +4,5 @@ set -e
 
 echo "> $0 $*"
 
-find $1 -type d -name "CVS" -exec rm -rf {} \; >/dev/null 2>&1 || true
-find $1 -type d -name ".svn" -exec rm -rf {} \; >/dev/null 2>&1 || true
 find $1 -type d -name ".bzr" -exec rm -rf {} \; >/dev/null 2>&1 || true
-find $1 -type f -name ".cvsignore" -exec rm {} \; >/dev/null 2>&1 || true
 find $1 -type f -name ".bzrignore" -exec rm {} \; >/dev/null 2>&1 || true
