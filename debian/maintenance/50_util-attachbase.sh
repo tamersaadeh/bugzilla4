5f20aed8e06b9245c0efd36bad0540dba8147666
Tamer Saadeh <tamersaadeh@gmail.com>
2012-02-17 21:06:33 +0100

    updated attachbase patch to 4.2

--- debian/maintenance/50_util-attachbase.sh
+++ debian/maintenance/50_util-attachbase.sh
@@ -10,14 +10,14 @@ cd "$1" && patch -p1 < "$0"
 
 exit 0
 
---- bugzilla-srcdir/Bugzilla/Util.pm-orig	2009-08-26 10:03:35.000000000 -0700
-+++ bugzilla-srcdir/Bugzilla/Util.pm	2009-08-26 10:04:00.000000000 -0700
-@@ -304,7 +304,7 @@
-sub use_attachbase {
-    my $attachbase = Bugzilla->params->{'attachment_base'};
+--- a/Bugzilla/Util.pm	2012-02-17 19:03:56 +0100
++++ b/Bugzilla/Util.pm	2012-02-17 19:03:56 +0100
+@@ -301,7 +301,7 @@ sub remote_ip {
+ 
+ sub use_attachbase {
+     my $attachbase = Bugzilla->params->{'attachment_base'};
 -    return ($attachbase ne ''
 +    return ($attachbase &&
-            && $attachbase ne Bugzilla->params->{'urlbase'}
-            && $attachbase ne Bugzilla->params->{'sslbase'}) ? 1 : 0;
-}
-
+             && $attachbase ne Bugzilla->params->{'urlbase'}
+             && $attachbase ne Bugzilla->params->{'sslbase'}) ? 1 : 0;
+ }
