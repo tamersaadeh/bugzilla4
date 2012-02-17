84e61951d13bdad53d695299bf5019aa1ca6a389
Tamer Saadeh <tamersaadeh@gmail.com>
2012-02-17 20:51:45 +0100

    patched Bugzilla/Install/Localconfig.pm to debainize it

--- Bugzilla/Install/Localconfig.pm
+++ Bugzilla/Install/Localconfig.pm
@@ -208,7 +208,7 @@ sub update_localconfig {
 
     my $output      = $params->{output} || 0;
     my $answer      = Bugzilla->installation_answers;
-    my $localconfig = read_localconfig('include deprecated');
+    my $localconfig = $params->{debian_localconfig} || read_localconfig('include deprecated');
 
     my @new_vars;
     foreach my $var (LOCALCONFIG_VARS) {
@@ -273,7 +273,7 @@ sub update_localconfig {
                                      ["*$name"]), "\n";
    }
 
-    if (@new_vars) {
+    if (!$params->{debian_localconfig} && @new_vars) {
         my $newstuff = join(', ', @new_vars);
         print "\n";
         print colored(install_string('lc_new_vars', { localconfig => $filename,
